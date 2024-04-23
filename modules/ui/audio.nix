8f0669e03e27a72724e27732c03be35c50f7e742
Andre <andre@8bitbuddhism.com>
2024-04-23 11:58:20 -0400

    Re-add Easy Effects

--- modules/ui/audio.nix
+++ modules/ui/audio.nix
@@ -7,9 +7,9 @@ with lib;
 {
 	options = {
 		host.ui.audio = {
-      enable = mkEnableOption (mdDoc "Enables audio");
-      enableLowLatency = mkEnableOption (mdDoc "Enables low-latency audio (may cause crackling) per https://nixos.wiki/wiki/PipeWire#Low-latency_setup ");
-    };
+			enable = mkEnableOption (mdDoc "Enables audio");
+			enableLowLatency = mkEnableOption (mdDoc "Enables low-latency audio (may cause crackling) per https://nixos.wiki/wiki/PipeWire#Low-latency_setup ");
+		};
 	};
 
 	config = mkIf cfg.enable {
@@ -29,7 +29,11 @@ with lib;
 			jack.enable = true;
 		};
 
-    # Reduce audio latency per https://nixos.wiki/wiki/PipeWire#Low-latency_setup
+		services.flatpak.packages =	mkIf config.host.ui.flatpak.enable [
+			"com.github.wwmm.easyeffects"	
+		];
+
+	    # Reduce audio latency per https://nixos.wiki/wiki/PipeWire#Low-latency_setup
 		services.pipewire.extraConfig.pipewire = mkIf cfg.enableLowLatency {
 			"92-low-latency.conf" = {
 				"context.properties" = {
