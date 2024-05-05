1552467793229d87b6e718b92abd16860a6c303a
Andre <andre@8bitbuddhism.com>
2024-05-05 11:27:34 -0400

    Shura - fingerprint support merged upstream

--- hosts/Shura/hardware-configuration.nix
+++ hosts/Shura/hardware-configuration.nix
@@ -64,15 +64,5 @@
 		hostName = "Shura";
 	};
 
-	# Add fingerprint detection
-	services.fprintd = {
-		enable = true;
-		package = pkgs.fprintd-tod;
-		tod = {
-			enable = true;
-			driver = pkgs.libfprint-2-tod1-elan;
-		};
-	};
-
 	nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
 }
