b816d821636f9d30be905af80fe578c25ce74b92
Andre <andre@8bitbuddhism.com>
2024-05-09 12:19:33 -0400

    Limit users who can run Nix

--- modules/base/nix.nix
+++ modules/base/nix.nix
@@ -19,8 +19,14 @@
       extra-substituters = [ "https://cache.lix.systems" ];
       trusted-public-keys = [ "cache.lix.systems:aBnZUw8zA7H35Cz2RyKFVs3H4PlGTLawyY5KRbvJR8o=" ];
 
+      # Only allow these users to use Nix
+      allowed-users = [
+        "root"
+        config.users.users.aires.name
+      ];
+
       # Avoid signature verification messages when doing remote builds
-      trusted-users = [ "${config.users.users.aires.name}" ];
+      trusted-users = [ config.users.users.aires.name ];
     };
 
     # Enable periodic nix store optimization
