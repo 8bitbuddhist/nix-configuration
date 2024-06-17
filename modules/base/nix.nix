a5d5047dbd64499f4cb6dc9c2701f5bbda5ec3ab
Andre <andre@8bitbuddhism.com>
2024-06-17 15:56:14 -0400

    Fix Lix cache

--- modules/base/nix.nix
+++ modules/base/nix.nix
@@ -16,7 +16,7 @@
       ];
 
       # Use Lix instead of Nix
-      extra-substituters = [ "https://cache.lix.systems" ];
+      substituters = [ "https://cache.lix.systems" ];
       trusted-public-keys = [ "cache.lix.systems:aBnZUw8zA7H35Cz2RyKFVs3H4PlGTLawyY5KRbvJR8o=" ];
 
       # Only allow these users to use Nix
