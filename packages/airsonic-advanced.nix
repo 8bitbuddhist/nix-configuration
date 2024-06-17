6582edd0b631dc3a55f001ea4550a8967cf6fa83
Andre <andre@8bitbuddhism.com>
2024-06-17 15:37:01 -0400

    Update Airsonic Advanced to 11.1.4-SNAPSHOT.20240616141843

--- packages/airsonic-advanced.nix
+++ packages/airsonic-advanced.nix
@@ -7,11 +7,11 @@
 
 stdenv.mkDerivation rec {
   pname = "airsonic-advanced";
-  version = "11.1.4-SNAPSHOT.20240531071418";
+  version = "11.1.4-SNAPSHOT.20240616141843";
 
   src = fetchurl {
     url = "https://github.com/kagemomiji/airsonic-advanced/releases/download/${version}/airsonic.war";
-    sha256 = "1668a41ad75c084dd9f1d745dd3d5611c19eb286d11cf86255f15e156ffc7163";
+    sha256 = "57877d56ab913974cfb5b5f3f8d2e2df4b289e7cde494f8766b7e35a93f82fbe";
   };
 
   buildCommand = ''
