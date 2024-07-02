c1308f9ef4f7fd06defc28b6db2db3b9cc5688a7
Andre <andre@8bitbuddhism.com>
2024-07-02 14:06:34 -0400

    Dimaga: add hostname

--- hosts/Dimaga/hardware-configuration.nix
+++ hosts/Dimaga/hardware-configuration.nix
@@ -73,8 +73,10 @@ in
   # (the default) this is the recommended approach. When using systemd-networkd it's
   # still possible to use this option, but it's recommended to use it in conjunction
   # with explicit per-interface declarations with `networking.interfaces.<interface>.useDHCP`.
-  networking.useDHCP = lib.mkDefault true;
-  # networking.interfaces.wlp2s0.useDHCP = lib.mkDefault true;
+  networking = {
+    hostName = "Dimaga";
+    useDHCP = lib.mkDefault true;
+  };
 
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
   hardware.cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
