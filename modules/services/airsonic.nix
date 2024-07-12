0d5b438ed151a14396dc91fc6c4ec91b7aaced8b
Andre <andre@8bitbuddhism.com>
2024-07-12 11:30:25 -0400

    Modules: remove problematic CSP policy for Airsonic (_still_ doesn't fix video playback)

--- modules/services/airsonic.nix
+++ modules/services/airsonic.nix
@@ -52,7 +52,6 @@ in
             proxy_set_header Host              $host;
             proxy_max_temp_file_size           0;
             proxy_ssl_server_name on;
-            add_header Content-Security-Policy "default-src 'self'; script-src 'self' 'unsafe-inline' 'unsafe-eval' www.gstatic.com; img-src 'self' *.akamaized.net; style-src 'self' 'unsafe-inline' fonts.googleapis.com; font-src 'self' fonts.gstatic.com; frame-src 'self'; object-src 'none'";
           '';
         };
       };
