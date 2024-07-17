7614f02d79587db4d4ac6930d07fd34a57e59c75
Andre <andre@8bitbuddhism.com>
2024-07-17 11:08:22 -0400

    Bluetooth: Add systemd service to enable adapter at boot

--- modules/system/bluetooth.nix
+++ modules/system/bluetooth.nix
@@ -33,5 +33,19 @@ in
 
     # Add Bluetooth LE audio support
     environment.systemPackages = with pkgs; [ liblc3 ];
+
+    # FIXME: Create systemd service to manually start the adapter on boot.
+    # This is a workaround for hardware.bluetooth.powerOnBoot not working as expected.
+    systemd.services.startBluetooth = {
+      description = "Manually starts the Bluetooth service on boot";
+      after = [ "bluetooth.target" ];
+      wantedBy = [ "multi-user.target" ];
+      serviceConfig = {
+        type = "simple";
+        ExecStart = "${pkgs.bluez}/bin/bluetoothctl -- power on";
+        Restart = "always";
+        RestartSec = "5s";
+      };
+    };
   };
 }
