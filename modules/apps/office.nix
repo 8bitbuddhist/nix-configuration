3c4b2f1424423854191624d2df9d5856e5ad7b65
Andre <andre@8bitbuddhism.com>
2024-07-30 16:25:29 -0400

    Apps: Downgrade Zoom due to screen share bug

--- modules/apps/office.nix
+++ modules/apps/office.nix
@@ -14,8 +14,16 @@ with lib;
       enable = true;
       packages = [
         "org.onlyoffice.desktopeditors"
-        "us.zoom.Zoom"
+        #"us.zoom.Zoom"
       ];
     };
+
+    # Downgrade Zoom due to https://github.com/flathub/us.zoom.Zoom/issues/471
+    services.flatpak.packages = [
+      {
+        appId = "us.zoom.Zoom";
+        commit = "b9505f108b5f9acb2bbad83ac66f97b42bc6a75b9c28ed7b75dec1040e013305";
+      }
+    ];
   };
 }
