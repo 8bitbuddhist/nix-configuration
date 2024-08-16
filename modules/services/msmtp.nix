0e27201be5d3583ce753dd1dc000f4c4e241639d
Andre <andre@8bitbuddhism.com>
2024-08-16 15:41:13 -0400

    Services: fix msmtp

--- modules/services/msmtp.nix
+++ modules/services/msmtp.nix
@@ -12,6 +12,7 @@ in
   config = lib.mkIf cfg.enable {
     programs.msmtp = {
       enable = true;
+      defaults.aliases = "/etc/aliases";
       accounts.default = {
         host = config.secrets.services.msmtp.host;
         user = config.secrets.services.msmtp.user;
@@ -21,8 +22,15 @@ in
         tls_starttls = true;
         port = 587;
         from = "${config.networking.hostName}@${config.secrets.networking.primaryDomain}";
-        to = config.secrets.users.aires.email;
       };
+    };
+
+    # Send all mail to my email address by default
+    environment.etc."aliases" = {
+      text = ''
+        default: ${config.secrets.users.aires.email}
+      '';
+      mode = "0644";
     };
   };
 }
