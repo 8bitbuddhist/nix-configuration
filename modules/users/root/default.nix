3a7a4a1b2dfaa89011e114945dfd1a64a927a20c
Andre <andre@8bitbuddhism.com>
2024-10-02 10:45:56 -0400

    Users: remove error when trying to run repo as root

--- modules/users/root/default.nix
+++ modules/users/root/default.nix
@@ -3,9 +3,14 @@
   # Give root user access to run remote builds
   home-manager.users.root = {
     home.stateVersion = "24.05";
-    programs.ssh = lib.mkIf config.nix.distributedBuilds {
-      enable = true;
-      matchBlocks = config.secrets.users.root.sshConfig;
+    programs = {
+      ssh = lib.mkIf config.nix.distributedBuilds {
+        enable = true;
+        matchBlocks = config.secrets.users.root.sshConfig;
+      };
+      git.extraConfig = {
+        safe.directory = "${config.secrets.nixConfigFolder}/.git";
+      };
     };
   };
 }
