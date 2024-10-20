845abac217b726123cb1c470cea5af2cee94d501
Andre <andre@8bitbuddhism.com>
2024-10-20 13:08:49 -0400

    Shura: remove bluetooth profiles

--- hosts/Shura/default.nix
+++ hosts/Shura/default.nix
@@ -4,17 +4,6 @@ let
   stateVersion = "24.05";
   hostName = "Shura";
 
-  # Copy bluetooth device configs
-  shure-aonic-bluetooth = pkgs.writeText "info" (
-    builtins.readFile ./bluetooth/shure-aonic-bluetooth-params
-  );
-  mano-touchpad-bluetooth = pkgs.writeText "info" (
-    builtins.readFile ./bluetooth/mano-touchpad-bluetooth-params
-  );
-  vitrix-pdp-pro-bluetooth = pkgs.writeText "info" (
-    builtins.readFile ./bluetooth/vitrix-pdp-pro-params
-  );
-
   # Use gremlin user's monitor configuration for GDM (desktop monitor primary). See https://discourse.nixos.org/t/gdm-monitor-configuration/6356/4
   monitorsXmlContent = builtins.readFile ./monitors.xml;
   monitorsConfig = pkgs.writeText "gdm_monitors.xml" monitorsXmlContent;
@@ -130,12 +119,6 @@ in
   systemd.tmpfiles.rules = [
     # Use gremlin user's monitor config for GDM (defined above)
     "L+ /run/gdm/.config/monitors.xml - - - - ${monitorsConfig}"
-
-    # Install Bluetooth device profiles
-    "d /var/lib/bluetooth/AC:50:DE:9F:AB:88/ 0700 root root" # First, make sure the directory exists
-    "L+ /var/lib/bluetooth/AC:50:DE:9F:AB:88/00:0E:DD:72:2F:0C/info - - - - ${shure-aonic-bluetooth}"
-    "L+ /var/lib/bluetooth/AC:50:DE:9F:AB:88/F8:5D:3C:7D:9A:00/info - - - - ${mano-touchpad-bluetooth}"
-    "L+ /var/lib/bluetooth/AC:50:DE:9F:AB:88/00:34:30:47:37:AB/info - - - - ${vitrix-pdp-pro-bluetooth}"
   ];
 
 }
