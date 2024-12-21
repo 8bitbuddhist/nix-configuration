b935836c779d95335eb3442c548bad6fceef7a4f
Andre <andre@8bitbuddhism.com>
2024-12-21 14:43:10 -0500

    Services: install tuned

--- modules/nixos/system/power/default.nix
+++ modules/nixos/system/power/default.nix
@@ -7,7 +7,7 @@
   ...
 }:
 let
-  cfg = config.${namespace};
+  cfg = config.${namespace}.powerManagement;
 
   ppd-patched = pkgs.power-profiles-daemon.overrideAttrs (
     _finalAttrs: _prevAttrs: {
@@ -26,10 +26,36 @@ in
   options.${namespace}.powerManagement.enable =
     lib.mkEnableOption "Enables power management, e.g. for laptops.";
 
-  # Configure power management via power-profiles-daemon
-  # https://gitlab.freedesktop.org/upower/power-profiles-daemon
-  config.services.power-profiles-daemon = lib.mkIf cfg.powerManagement.enable {
-    enable = true;
-    package = ppd-patched;
+  config = {
+    # Configure power management via power-profiles-daemon
+    # https://gitlab.freedesktop.org/upower/power-profiles-daemon
+    services.power-profiles-daemon = lib.mkIf cfg.enable {
+      enable = true;
+      package = ppd-patched;
+    };
+
+    # Configure power management via tuned
+    # https://github.com/redhat-performance/tuned/
+    environment.systemPackages = [ pkgs.${namespace}.tuned ];
+    systemd.services.tuned = {
+      wantedBy = [ "multi-user.target" ];
+      after = [ "dbus.service" ];
+      description = "tuned power management daemon.";
+      enable = true;
+      serviceConfig = {
+        Type = "simple";
+        ExecStart = "${pkgs.${namespace}.tuned}/bin/tuned";
+      };
+    };
+    systemd.services.tuned-ppd = {
+      wantedBy = [ "multi-user.target" ];
+      after = [ "tuned.service" ];
+      description = "tuned power management daemon - power-profiles-daemon compatibility.";
+      enable = true;
+      serviceConfig = {
+        Type = "simple";
+        ExecStart = "${pkgs.${namespace}.tuned}/bin/tuned-ppd";
+      };
+    };
   };
 }
