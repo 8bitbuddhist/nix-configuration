926634b996f69238d72aef14f851bbfea3432a07
Andre <andre@8bitbuddhism.com>
2025-01-14 16:09:41 +0000

    Pihole: bugsquashing

--- systems/aarch64-linux/Pihole/default.nix
+++ systems/aarch64-linux/Pihole/default.nix
@@ -27,11 +27,18 @@ let
 
     # mount local storage
     if [ ! -f /dev/mapper/sda ]; then
-      echo "Mounting home:"
+      echo "Mounting sda:"
       mount -o relatime /dev/mapper/sda /sda
 
       if [ $? -ne "0" ]; then
         echo "Failed to mount @home"
+      fi
+
+      echo "Mounting hevana:"
+      mount /Hevana
+
+      if [ $? -ne "0" ]; then
+        echo "Failed to mount hevana"
       fi
 
       echo "Mounting swap:"
